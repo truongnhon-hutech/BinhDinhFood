e2a9b0e128fd616e9d50e4e50d5def9a8928d288
truongnhon-hutech <vothuongtruongnhon2002@gmail.com>
2022-08-31 13:15:36 +0700

    preloading

---
+++ BinhDinhFoodWeb/Views/Shared/Components/PreLoadingComponent/PreLoadingComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BinhDinhFoodWeb.Views.User.Components.PreLoadingComponent
+{
+    public class PreLoadingComponent :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
